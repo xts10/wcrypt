a6454077f7068c8158880ae8910f4c9cae0d8477
sp4rk2 <sp4rk2sp4rk2@gmail.com>
2017-05-16 12:41:33 +0100

    Update wcryptProfit.py

--- wcryptProfit.py
+++ wcryptProfit.py
@@ -12,9 +12,7 @@ def main():
     #Place addresses in this array.
     address = [ "12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw",
                 "115p7UMMngoj1pMvkpHijcRdfJNXj6LrLn",
-                "13AM4VW2dhxYgXeQepoHkHSQuy6NgaEb94",
-                "1QAc9S5EmycqjzzWDc1yiWzr9jJLC8sLiY",
-                "15zGqZCTcys6eCjDkE3DypCjXi6QWRV6V1"]
+                "13AM4VW2dhxYgXeQepoHkHSQuy6NgaEb94"]
  
     #Defines empty variable to calculate total.
     totalBTC=0
